ec6b488ce19a736407a24d448a64e164ab4e7a1c
Pallavi <pallavishettigar03@gmail.com>
2024-01-12 22:06:53 +0530

    add changes

---
+++ script.js
@@ -0,0 +1,5 @@
+const wrapper = document.querySelector(".wrapper"),
+generateBtn = wrapper.querySelector(".form button ");
+generateBtn.addEventListener("click" , () => {
+    wrapper.classList.add("active");
+})
